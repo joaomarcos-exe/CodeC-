ccaa6bacd97ff6444da9e18ae566f34ca243292e
João Marcos <joaomarcos-icm@hotmail.com>
2021-08-11 07:34:35 -0300

    fibonacci and factorial

---
+++ Fibonacci/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,40 @@
+// Fibonacci.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void fibo(int num, int res, int cont);
+
+int main()
+{
+	cout << "Fibonacci\n";
+	cout << 1 << "\n";
+	fibo(1, 1, 100);
+}
+
+void fibo(int num, int res, int cont) {
+	if (res < cont)
+	{
+		res = num + res;
+		num = res - num;
+		cout << res << "\n";
+		fibo(num, res, cont);
+	}
+	else {
+		cout << res << "\nThe end\n";
+	}
+}
+
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
